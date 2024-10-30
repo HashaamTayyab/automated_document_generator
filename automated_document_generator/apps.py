55f902f5dc40770c6c86b52bd9934fc1207603cf
Hashaam Tayyab <hasham.chudery786@gmail.com>
2024-10-30 17:29:24 +0500

    Created the project structure to work on

---
+++ automated_document_generator/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AutomatedDocumentGeneratorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'automated_document_generator'
